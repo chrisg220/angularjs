659f1d5d1f796a845fb004c438121aac278bea15
Christopher Gonzales <chrisg220@hotmail.com>
2013-07-24 00:14:55 -0700

    Thinking differently about organization

--- egghead/js/app.js
+++ egghead/js/app.js
@@ -1,11 +1,23 @@
 // Define an App
 var app = angular.module('phoneApp', []);
 
-app.controller('AppCtrl', function($scope) {
-	this.sayHi = function() {			// assign 'sayHi' to controller itself
-		alert('Hello There!');
-	}
+var phoneAppStuff = {};
 
-	return $scope.AppCtrl = this; 		// assign 'AppCtrl' property on '$scope' to 'this'. Return reference.
-});
+phoneAppStuff.controllers = {};
 
+phoneAppStuff.controllers.AppCtrl = function($scope) {
+	this.sayHi = function() {
+		alert('hi');
+	};
+	return $scope.AppCtrl = this;
+};
+
+phoneAppStuff.directives = {};
+phoneAppStuff.directives.panel = function() {
+	return {
+		restrict: "E"
+	};
+};
+
+app.directive(phoneAppStuff.directives);
+app.controller(phoneAppStuff.controllers);
