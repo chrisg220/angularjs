f7303caaa82fca4e8e66e2682b590b729fe4da48
Christopher Gonzales <chrisg220@hotmail.com>
2013-08-11 19:53:11 -0700

    added branch 'zippy_directive'. (transclusion, ng-show, templates)

--- egghead/js/app.js
+++ egghead/js/app.js
@@ -1,18 +1,31 @@
 // Define an App
 var app = angular.module('app', []);
 
-// Declaring dependencies before function() protects against minification variable collision.
-// Dependencies are injected in the same order they are declared.
-app.controller('MyCtrl', ['$scope', '$http', function(a, b) {
-  console.log(b);
-}]);
-
-app.directive('myDirective', function($http, $parse) {
+app.directive('zippy', function() {
   return {
-    link: function(scope, elem, attrs) {
-      console.log(scope)
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      attrfoo: '@',
+      attrbar: '@'
+    },
+    template: '<div>' +
+                '<h3 ng-click="toggleContent()"> First input is: {{attrfoo}} </h3><br/>' +
+                  'Second input is: {{attrbar}} <br/>' +
+                '<div ng-click="myFunc()" ng-show="isContentVisible" ng-transclude> </div>' +
+              '</div>',
+
+    link: function(scope) {
+      scope.isContentVisible = false;
+
+      scope.toggleContent = function() {
+        scope.isContentVisible = !scope.isContentVisible;
+        console.log('toggleContent Fired');
+      };
+
+      scope.myFunc = function() {
+        console.log('myfunct Fired')
+      };
     }
   };
 });
-
-
