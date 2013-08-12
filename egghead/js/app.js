1b611dee86c4bf869e4eb58b73ff8c479105af62
Christopher Gonzales <chrisg220@hotmail.com>
2013-08-11 23:44:37 -0700

    add 'directive_communication' branch. Children directives calling parent directives' controllers

--- egghead/js/app.js
+++ egghead/js/app.js
@@ -1,25 +1,37 @@
 // Define an App
 var app = angular.module('app', []);
 
-app.controller('AppCtrl', function($scope, $q) {
+app.directive('country', function() {
+  return {
+    restrict: 'E',
+    controller: function() {
+      this.makeAnnouncement = function(message) {
+        console.log('Country says: ' + message);
+      }
+    }
+  };
+});
 
-  var defer = $q.defer();
+app.directive('state', function() {
+  return {
+    restrict: 'E',
+    require: '^country',
+    controller: function() {
+      this.makeLaw = function(law) {
+        console.log('Law: ' + law);
+      }
+    },
+    link: function(scope, element, attrs, countryCtrl) {}
+  };
+});
 
-  defer.promise
-    .then(function(weapon) {
-      alert('You can have my ' + weapon);
-
-      return 'bow';
-    })
-    .then(function(weapon) {
-      alert('And my ' + weapon);
-
-      return 'axe';
-    })
-    .then(function(weapon) {
-      alert('As well as my ' + weapon);
-    })
-
-  defer.resolve('sword');
-
+app.directive('city', function() {
+  return {
+    restrict: 'E',
+    require: ['^country', '^state'],
+    link: function(scope, element, attrs, ctrls) {
+      ctrls[0].makeAnnouncement('from city');
+      ctrls[1].makeLaw('Jump higher');
+    }
+  };
 });
