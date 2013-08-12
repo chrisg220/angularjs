6d8dc8b15a27308781dc874ced96cb2e730cd024
Christopher Gonzales <chrisg220@hotmail.com>
2013-08-11 19:27:00 -0700

    added branch DOLLARscope vs scope

--- egghead/js/app.js
+++ egghead/js/app.js
@@ -1,33 +1,16 @@
 // Define an App
 var app = angular.module('app', []);
 
-app.directive('dumbPassword', function() {
-  var validElement = angular.element('<div>{{model.input}}</div>');
+// Declaring dependencies before function() protects against minification variable collision.
+// Dependencies are injected in the same order they are declared.
+app.controller('MyCtrl', ['$scope', '$http', function(a, b) {
+  console.log(b);
+}]);
 
-  // Could be expanded for validations
-  var link = function(scope, elem) {
-    scope.$watch('model.input', function(value) {
-      if(value === 'secretword') {
-        validElement.toggleClass('button');
-        elem.append(validElement);
-      }
-      else {
-        validElement.removeClass('button');
-        validElement.remove();
-      }
-    });
-  };
-
+app.directive('myDirective', function($http, $parse) {
   return {
-    restrict: 'E',
-    replace: true,
-    template: '<div> \n <input type=\"text\" ng-model=\"model.input\"> \n  </div>',
-
-    compile: function(tElem) {
-      // Appends angular element onto template upon compilation process.
-      // No need to traverse DOM. Reference to element is stored
-      // tElem.append(validElement);
-      return link;
+    link: function(scope, elem, attrs) {
+      console.log(scope)
     }
   };
 });
